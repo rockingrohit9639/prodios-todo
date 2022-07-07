1db9b0c87dc3216dfdc9baf0452c911db7be4859
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-07 18:16:10 +0530

    Added styling and input fields

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
-      Home
+      <main className="home">
+        <div className="home__bg">
+          <div className="overlay"></div>
+          <img src="/assets/bg.jpg" alt="bg" className="bg__img" />
+        </div>
+
+        <div className="home__content">
+          <h1 className="heading">prodios todo</h1>
+
+          <form className="inputForm">
+            <input
+              type="text"
+              className="input inputForm__title"
+              placeholder="Todo Title"
+            />
+
+            <textarea
+              rows="4"
+              className="input inputForm__desc"
+              placeholder="Todo Description"
+            ></textarea>
+
+            <button className="inputForm__button">Add</button>
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
